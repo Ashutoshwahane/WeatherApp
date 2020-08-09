90f046b704658e51a25ca7cd820afb3515e44440
Ashutoshwahane <ashutosh.wahane10@gmail.com>
2020-08-09 18:24:35 +0530

    splash activity

--- app/src/main/java/com/cybernerd/weatherapp/view/SplashActivity.kt
+++ app/src/main/java/com/cybernerd/weatherapp/view/SplashActivity.kt
@@ -1,7 +1,9 @@
 package com.cybernerd.weatherapp.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import com.cybernerd.weatherapp.R
 
 class SplashActivity : AppCompatActivity() {
@@ -9,5 +11,10 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, SearchActivity::class.java))
+            finish()
+        },2000)
     }
 }
