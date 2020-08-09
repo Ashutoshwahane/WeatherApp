82732d0ca4fdf9952634109b7d4a1e7ed0b76268
Ashutoshwahane <ashutosh.wahane10@gmail.com>
2020-08-09 18:26:52 +0530

    create a funtion to call repository function and init viewmodel variable

--- app/src/main/java/com/cybernerd/weatherapp/viewModel/SearchActivityViewModel.kt
+++ app/src/main/java/com/cybernerd/weatherapp/viewModel/SearchActivityViewModel.kt
@@ -4,19 +4,26 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import com.cybernerd.weatherapp.model.Location
 import com.cybernerd.weatherapp.repository.SearchActivityRepository
 
 class SearchActivityViewModel(application: Application): AndroidViewModel(application) {
 
-    val repository = SearchActivityRepository(application)
+   private val repository = SearchActivityRepository(application)
     val showProgress : LiveData<Boolean>
+    val locationList : LiveData<List<Location>>
 
     init {
         this.showProgress = repository.showProgress
+        this.locationList = repository.locationList
     }
 
     fun changeProgress(){
         repository.changeState()
+    }
+
+    fun searchLocation(searchString: String){
+        repository.searchLocation(searchString)
     }
 
 }
