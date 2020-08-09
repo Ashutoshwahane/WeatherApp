cc0f328f0eec19c80f542c0753592e5a37e40db3
Ashutoshwahane <ashutosh.wahane10@gmail.com>
2020-08-09 18:23:52 +0530

    add recycleview and getting entered location

--- app/src/main/java/com/cybernerd/weatherapp/view/SearchActivity.kt
+++ app/src/main/java/com/cybernerd/weatherapp/view/SearchActivity.kt
@@ -7,12 +7,14 @@ import android.view.View.VISIBLE
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.cybernerd.weatherapp.R
+import com.cybernerd.weatherapp.adapter.LocationAdapter
 import com.cybernerd.weatherapp.viewModel.SearchActivityViewModel
 import kotlinx.android.synthetic.main.activity_search.*
 
 class SearchActivity : AppCompatActivity() {
 
     private lateinit var searchActivityViewModel: SearchActivityViewModel
+    private lateinit var locationAdapter: LocationAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,7 +23,10 @@ class SearchActivity : AppCompatActivity() {
         searchActivityViewModel = ViewModelProvider(this).get(SearchActivityViewModel::class.java)
 
         iv_search.setOnClickListener {
-            searchActivityViewModel.changeProgress()
+            if (et_search.text!!.isNotEmpty()) {
+                searchActivityViewModel.searchLocation(et_search.text.toString())
+            }
+
         }
 
         searchActivityViewModel.showProgress.observe(this, Observer {
@@ -31,5 +36,16 @@ class SearchActivity : AppCompatActivity() {
                 search_progressbar.visibility = GONE
             }
         })
+
+
+        searchActivityViewModel.locationList.observe(this, Observer {
+            locationAdapter.setLocation(it)
+        })
+        locationAdapter = LocationAdapter(this)
+        recycleview.adapter = locationAdapter
+
+
+
+
     }
 }
